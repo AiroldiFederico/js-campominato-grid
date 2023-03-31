dc58ac4920df4cf92ed70399e36d4b30a7951812
Federico Airoldi <airoldibusiness@gmail.com>
2023-04-01 01:28:41 +0200

    FASE 1 - predisposizione struttura html e css

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,150 +1,18 @@
+
+/*---------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 /*
-CONSEGNA:
-Palindroma
-Chiedere all’utente di inserire una parola
-Creare una funzione per capire se la parola inserita è palindroma
-
-
+Consegna
+L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+Bonus
+Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+- con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+- con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+- con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 */
 
-
-/* STEP:
-
-1 - Utente inserisce la parola
-
-2 - Prendo il valore dell'input registrandolo in una variabile
-
-3 - Avvio la sequenza tramite un bottone, quindi registro anche quel input
-
-4 - Creo la funzione che al click del bottone raccoglie l'input della parola, e stampi la funzione 
-
-5 - Creo una funzione con un ciclo for, il ciclo for partirà al contrario e durerà per la lunghezza della parola
-
-6 - alla funzione che raccoglie l'input e stampa la funziona ora gli faccio paragonare le due stringhe
-
-*/
-
-
-//inputraw
-//let  = raw.toLowerCase();
-
-//input parola inserita dall'utente
-// let InputPalindrome = document.getElementById('InputPali');
-
-
-
-// //Tasto per verificare
-// let Button1 = document.getElementById('btn1');
-
-// //Al click raccolgo i dati
-// Button1.addEventListener("click", function() {
-
-//     InputPalindrome = document.getElementById('InputPali').value;
-
-
-
-//     if ( InputPalindrome === verPali(InputPalindrome)) {
-
-//         //risultatato positivo
-//         document.getElementById("OutputPali").innerHTML = 'Complimenti, è palindroma!';
-//     } else {
-
-//         //risultato negativo
-//         document.getElementById("OutputPali").innerHTML = 'Purtroppo non è palindroma';
-//     }
-//     //document.getElementById("OutputPali").innerHTML = verPali(InputPalindrome);
-
-//     //console.log(InputPalindrome);
-//     //console.log(verPali(InputPalindrome));
-// });
-
-    
-    
-// // funzione che trascrive la parola al contrario
-// function verPali(word) {
-    
-//     let RevWord = "";
-    
-//     //ad ogni ciclo stampo l'ultima lettera
-//     for (let i = word.length - 1; i >= 0; i--) {
-//         RevWord += word[i];
-//     }
-//     console.log(word.lenght);
-//     console.log(RevWord);
-//     return RevWord;
-    
-// };
-
-
-// /*------------------------------------------------------------------------*/
-// /*
-// Pari e Dispari
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// Sommiamo i due numeri
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// Dichiariamo chi ha vinto. */
-
-
-// /*
-
-// STEP:
-
-// 1. Sistemo la struttura html
-
-// 2. Raccolgo gli input
-
-// 3. Selezione click button
-
-// 4. Evento al click
-
-// 5. Genero un numero casuale per la cpu e la stampo nel placeholder
-
-// 6. Faccio la somma del numero CPU e Utente
-
-// */
-
-
-// //input even odd
-// let EvenOrOdd = document.getElementById('InputEO');
-
-// //input numero
-// let InputNumero = document.getElementById('Inumero');
-
-// //Tasto per verificare
-// let Button2 = document.getElementById('btn2');
-
-
-// //evento al click
-// Button2.addEventListener("click", function() {
-
-//     //genero un numero casuale
-//     let numeroCasuale = Math.floor(Math.random() * 5) + 1;
-
-//     //punto al elemento html 
-//     let numeroCPU = document.getElementById('disabledTextInput');
-
-//     //scrivo il numero come placeholder
-//     numeroCPU.setAttribute("placeholder", numeroCasuale);
-
-//     EvenOrOdd.value;
-//     InputNumero.value;
-
-//     let somma = parseInt(numeroCasuale) + parseInt(InputNumero.value);
-
-//     if ( EvenOrOdd.value == 1 && somma % 2 == 0 || EvenOrOdd.value == 2 && somma % 2 !== 0 ) {
-//         //risultatato positivo
-//         document.getElementById("OutputEvenOdd").innerHTML = `Complimenti, hai vinto! il risultato è ${somma}`;
-//     } else {
-//         //risultatato negativo
-//         document.getElementById("OutputEvenOdd").innerHTML = `Hai perso! il risultato è ${somma}`;
-//     }
-
-   
-    
-//     console.log(somma);
-
-// });
-
+/*---------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
 
